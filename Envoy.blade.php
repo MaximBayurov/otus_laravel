291d8c6e795e81f7699fb813c33f120fec754340
Maxim Bayurov <vakakak20@gmail.com>
2024-02-10 13:57:46 +0300

    homework 15

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,32 +1,28 @@
-@servers(['web' => ['maxim@192.168.0.159']])
-
-@setup
-    $projectRoot = '~/test/Laravel';
-@endsetup
+@servers(['web' => 'maxim@192.168.0.159'])
 
 @story('deploy')
-    sail-down
     update-code
     update-dependencies
     run-migrations
-    key-generate
     run-tests
-    sail-up
 @endstory
 
-@task('sail-down')
-    cd {{ $projectRoot }}
-    ./vendor/bin/sail stop
-@endtask
-
-@task('sail-up')
-    cd {{ $projectRoot }}
-    ./vendor/bin/sail up
-@endtask
+@error
+    if ($task === 'run-tests') {
+        rollback-migrations
+        rollback-code
+        update-dependencies
+    }
+@enderror
 
 @task('update-code')
     cd {{ $projectRoot }}
-    git pull origin {{ $branch }}
+    git pull origin-with-creds {{ $branch }}
+@endtask
+
+@task('rollback-code')
+    cd {{ $projectRoot }}
+    git pull origin-with-creds master -r
 @endtask
 
 @task('update-dependencies')
@@ -39,9 +35,9 @@
     ./vendor/bin/sail artisan migrate
 @endtask
 
-@task('key-generate')
+@task('rollback-migrations')
     cd {{ $projectRoot }}
-    ./vendor/bin/sail artisan key:generate
+    ./vendor/bin/sail artisan migrate:rollback
 @endtask
 
 @task('run-tests')
